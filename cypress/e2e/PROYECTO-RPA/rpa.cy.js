e6d81cb8042bc81b8e44a9633c3545177f898f7c
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-11-04 16:10:38 -0300

    capturar precio actual y anterior y enviarlo en el mensaje

--- cypress/e2e/PROYECTO-RPA/rpa.cy.js
+++ cypress/e2e/PROYECTO-RPA/rpa.cy.js
@@ -2,17 +2,20 @@
 
 let availability = "";
 let actualPrice = 0;
-let lastPrice = 0;
+let lastPriceQuery = 0;
+let lastDateQuery = "";
 const URL = Cypress.env("url");
 
 context('Acciones', () => {
     it("Visitar pagina", ()=>
         {
-        lastPrice = cy.readFile('cypress/e2e/PROYECTO-RPA/datos.json');
-        cy.log(lastPrice);
-        cy.log(URL);
-        cy.visit(URL);
+        cy.readFile('cypress/e2e/PROYECTO-RPA/datos.json').then((data) => {
+            lastPriceQuery = data.lastPrice;
+            lastDateQuery = data.date;
+        });
         
+        cy.visit(URL);
+
         cy.get('[class^="stockDetalle"]')
             .find('span')  
             .invoke('text')        
@@ -27,6 +30,7 @@ context('Acciones', () => {
             .invoke('text')
             .then((text) => {
                 const numericPrice = parseInt(text.trim().replace(/[^0-9]/g, ''), 10);
+                actualPrice = numericPrice;
                 const date = new Date();
                 const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
                 cy.writeFile('cypress/e2e/PROYECTO-RPA/datos.json', { lastPrice: numericPrice, availability: availability, url: URL, date: formattedDate });
@@ -37,18 +41,18 @@ context('Acciones', () => {
     it("Enviar mensaje telegram", ()=>
     {
         cy.log(availability);
-        // cy.request({
-        //     method: 'POST',
-        //     url: 'https://api.telegram.org/bot'+Cypress.env("BOT")+'/sendMessage',
-        //     form: true,
-        //     body: {
-        //         chat_id: Cypress.env("CHAT_ID"),
-        //         text: `Hola, el producto esta en estado: ${availability}`
-        //     }
-        // }).then((response) => {
-        //     expect(response.status).to.eq(200);
-        //     expect(response.body.ok).to.eq(true);
-        // }
-        // );
+        cy.request({
+            method: 'POST',
+            url: 'https://api.telegram.org/bot'+Cypress.env("BOT")+'/sendMessage',
+            form: true,
+            body: {
+                chat_id: Cypress.env("CHAT_ID"),
+                text: `Hola, el producto esta en estado: ${availability}. El precio actual es: ${actualPrice}. El precio anterior al ${lastDateQuery} era: ${lastPriceQuery}.`
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.ok).to.eq(true);
+        }
+        );
     });
 });
